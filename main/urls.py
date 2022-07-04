655e49f37fcd4681b182d397f10706e5c4c3cda3
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-04 17:19:24 +0200

    write iot info

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from main import views
 
 urlpatterns = [
-    path('monitoring/', views.show_monitoring, name='show_monitoring'),
-    path('monitoring/<int:mon_id>', views.show_info, name='show_info'),
-    path('monitoring/new', views.take_info, name='take_info')
+    path('', views.MonListView.as_view(), name='mon-list'),
+    path('new/', views.MonCreateView.as_view(), name='mon-create'),
+    path('<int:pk>/delete', views.MonDeleteView.as_view(), name='mon-del'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,37 +1,27 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Monitoring, Sensors
-from django.db.models import ObjectDoesNotExist
-import datetime
+from django.utils import timezone
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse_lazy
 
 
-def show_monitoring(requests):
-    mon = Monitoring.objects.all()
-    s = ''
-    for m in mon:
-        s += f'<p>{m.sensor} {m.date} {m.value}</p>'
-
-    return HttpResponse(s)
+class MonListView(ListView):
+    model = Monitoring
+    queryset = Monitoring.objects.all().order_by('-date')
+    template_name = 'main_list.html'
 
 
-def show_info(requests, mon_id):
-    try:
-        data = Monitoring.objects.get(pk=mon_id)
-        return HttpResponse(str(data))
+class MonCreateView(CreateView):
+    model = Monitoring
+    fields = ['sensor', 'value', 'date']
+    template_name = 'main_create.html'
 
-    except ObjectDoesNotExist:
-        print('ex')
-        return HttpResponseRedirect('/monitoring')
+    def get_initial(self):
+        return {'date': timezone.now()}
 
-def take_info(requests):
-    sensor = requests.GET.get("s", None)
-    value = requests.GET.get("v", None)
-    if value and sensor:
-        date = datetime.datetime.now()
-        print(date, sensor, value)
-        sensor = Sensors.objects.filter(name=sensor.upper())
-        print(sensor.sensorss_name)
-        m = Monitoring(2, value=value, date=date)
-        m.save()
-        return HttpResponse('OK')
-    else:
-        return HttpResponse('Not data')
+
+class MonDeleteView(DeleteView):
+    model = Monitoring
+    success_url = reverse_lazy('mon-list')
+    template_name = 'main-del.html'
